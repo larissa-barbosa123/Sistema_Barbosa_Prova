c41771510a40d822863c7cfa6d4817a076069810
Larissa De Almeida Barbosa BARBOSA <u07862281136@acad.pp.ifms.edu.br>
2026-04-14 13:11:54 -0400

    Comecei a fazer.

---
+++ src/sistema_barbosa_prova/Sistema_Barbosa_Prova.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema_barbosa_prova;
+
+/**
+ *
+ * @author u07862281136
+ */
+public class Sistema_Barbosa_Prova {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
